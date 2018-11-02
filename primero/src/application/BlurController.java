1bf2ef7ed976c481ed86998f9d0e91a56d6cea4e
luiscaguana <luiscaguana@10.34.179.140>
2018-11-02 11:50:40 +0100

    initial

---
+++ primero/src/application/BlurController.java
@@ -0,0 +1,27 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Ellipse;
+
+public class BlurController {
+
+
+	@FXML
+    private ImageView imageView;   
+	
+
+	@FXML
+    public void setblur() {           
+             imageView.setEffect(new GaussianBlur(10));
+    }
+	
+	@FXML
+	private void handleClose() {
+	
+	        System.exit(0);
+	}
+	
+	
+}
